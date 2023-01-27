58ba8da4fdc7c60671af1c7eaeee6552d1a3381d
emredursun <info.emredursun@gmail.com>
2023-01-28 00:40:08 +0100

    update Runner class

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -16,7 +16,7 @@ import org.junit.runner.RunWith;
         },
         features = "./src/test/resources/features",
         glue = {"stepdefinitions", "hooks"},
-        tags = "@api",
+        tags = "",
         dryRun = false
 )
 public class Runner {
